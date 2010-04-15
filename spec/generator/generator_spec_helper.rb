23e139844f5f65e9ef48bf810122871e596edfa2
lars <larsburgess@gmail.com>
2010-04-15 10:00:39 -0700

    re-animating the server specs

--- spec/generator/generator_spec_helper.rb
+++ spec/generator/generator_spec_helper.rb
@@ -1,5 +1,6 @@
 require 'templater/spec/helpers'
-
+$:.unshift File.join(__FILE__,'..','lib')
+require 'rhosync'
 require File.join(File.dirname(__FILE__),'..','..','generators','rhosync')
 
 Spec::Runner.configure do |config|

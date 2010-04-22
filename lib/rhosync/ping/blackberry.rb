8eea23046c9ee5e0d9a6c83554f126fc9c63b707
lars <larsburgess@gmail.com>
2010-04-21 18:19:45 -0700

    #3093167 - implement blackberry push

--- lib/rhosync/ping/blackberry.rb
+++ lib/rhosync/ping/blackberry.rb
@@ -1,7 +1,56 @@
+require 'net/http'
+require 'uri'
 module Rhosync
   class Blackberry
     def self.ping(params)
-      
+      begin
+        settings = get_config(Rhosync.base_directory)[Rhosync.environment]
+        host = settings[:mdsserver]
+      	port = settings[:mdsserverport]
+        
+        headers = { "X-WAP-APPLICATION-ID" => "/",
+                    "X-RIM-PUSH-DEST-PORT" => params['device_port'],
+                    "CONTENT-TYPE" => 'multipart/related; type="application/xml"; boundary=asdlfkjiurwghasf'}
+                    
+        Net::HTTP.new(host,port).start do |http|
+          request = Net::HTTP::Post.new('/pap',headers)
+          request.body = pap_message(params)
+          http.request(request)
+        end
+
+      rescue Exception => error
+        Logger.error "Error while sending ping: #{error}"
+  		  #Logger.error error.backtrace.join("\n")
+  		  raise error
+      end
+    end
+    
+    def self.pap_message(params)
+      data = "do_sync=" + (params['sources'] ? params['sources'].join(',') : "") + "\r\n"
+      data << "show_popup=#{params['message']}\r\n" if params['message']
+      data << "vibrate=#{params['vibrate']}\r\n" if params['vibrate']
+      post_body = <<-DESC
+--asdlfkjiurwghasf
+Content-Type: application/xml; charset=UTF-8
+
+<?xml version="1.0"?>
+<!DOCTYPE pap PUBLIC "-//WAPFORUM//DTD PAP 2.0//EN" 
+  "http://www.wapforum.org/DTD/pap_2.0.dtd" 
+  [<?wap-pap-ver supported-versions="2.0"?>]>
+<pap>
+<push-message push-id="pushID:#{(rand * 100000000).to_i.to_s}" ppg-notify-requested-to="http://localhost:7778">
+
+<address address-value="WAPPUSH=#{params['device_pin'].to_i.to_s(base=16).upcase}%3A100/TYPE=USER@rim.net"/>
+<quality-of-service delivery-method="preferconfirmed"/>
+</push-message>
+</pap>
+--asdlfkjiurwghasf
+Content-Type: text/plain
+
+#{data}
+--asdlfkjiurwghasf--
+DESC
+      post_body.gsub!(/\n/,"\r\n")
     end
   end
 end
---
+++ spec/ping/blackberry_spec.rb
@@ -0,0 +1,62 @@
+require File.join(File.dirname(__FILE__),'..','spec_helper')
+
+describe "Ping Blackberry" do
+  it_should_behave_like "SpecBootstrapHelper"
+  it_should_behave_like "SourceAdapterHelper"
+  
+  before do
+    @params = {"user_id" => @u.id, "api_token" => @api_token,
+      "sources" => [@s.name], "message" => 'hello world', 
+      "vibrate" => '5', "badge" => '5', "sound" => 'hello.mp3',
+      "device_pin" => @c.device_pin, "device_port" => @c.device_port}
+    post = mock('post')
+    post.stub!(:new).and_return(post)
+    post.stub!(:body=)
+    Net::HTTP::Post.stub!(:new).and_return(post)
+    
+    @http = mock('http')
+    @http.stub!(:request)
+    @http.stub!(:start).and_yield(@http)
+    Net::HTTP.stub!(:new).and_return(@http)
+  end
+  
+  it "should ping blackberry" do
+    Blackberry.ping(@params)
+  end
+  
+  it "should ping blackberry with connection error" do
+    error = 'Connection refused'
+    @http.stub!(:request).and_return { raise SocketError.new(error) }
+    Logger.should_receive(:error).once.with("Error while sending ping: #{error}")
+    lambda { Blackberry.ping(@params) }.should raise_error(SocketError,error)
+  end
+  
+  it "should compute pap_message" do
+    expected = <<PAP
+--asdlfkjiurwghasf
+Content-Type: application/xml; charset=UTF-8
+  
+<?xml version="1.0"?>
+<!DOCTYPE pap PUBLIC "-//WAPFORUM//DTD PAP 2.0//EN" 
+  "http://www.wapforum.org/DTD/pap_2.0.dtd" 
+  [<?wap-pap-ver supported-versions="2.0"?>]>
+<pap>
+<push-message push-id="pushID:RAND_ID" ppg-notify-requested-to="http://localhost:7778">
+
+<address address-value="WAPPUSH=0%3A100/TYPE=USER@rim.net"/>
+<quality-of-service delivery-method="preferconfirmed"/>
+</push-message>
+</pap>
+--asdlfkjiurwghasf
+Content-Type: text/plain
+
+do_sync=SampleAdapter
+show_popup=hello world
+vibrate=5
+
+--asdlfkjiurwghasf-- 
+PAP
+    actual = Blackberry.pap_message(@params).gsub!(/pushID\:\d+/,'pushID:RAND_ID')
+    actual.gsub!(/\r|\n|\s/,"").should == expected.gsub!(/\r|\n|\s/,"")
+  end
+end

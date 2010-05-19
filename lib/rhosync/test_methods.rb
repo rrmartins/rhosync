28128b2ecf35085a2c313f659acb529cb62736e2
lars <larsburgess@gmail.com>
2010-05-18 22:37:54 -0700

    rdoc for TestMethods

--- lib/rhosync/test_methods.rb
+++ lib/rhosync/test_methods.rb
@@ -4,6 +4,8 @@ module Rhosync
   end
   
   module TestMethods
+    # Initializes the source adapter under test for a given user, typically in a before(:each) block
+    # setup_test_for(Product,'testuser') #=> 'testuser' will be used by rest of the specs
     def setup_test_for(adapter,user_id)
       app_id = 'application'
       s_fields = {
@@ -23,15 +25,51 @@ module Rhosync
       @ss = SourceSync.new(@s)
     end
     
+    # Executes the adapter's query method and returns 
+    # the master document (:md) stored in redis
+    # For example, if your source adapter query method was:
+    # def query(params=nil)
+    #   @result = { 
+    #     "1"=>{"name"=>"Acme", "industry"=>"Electronics"},
+    #     "2"=>{"name"=>"Best", "industry"=>"Software"}
+    #   }
+    # end
+    # 
+    # test_query would return:
+    # { 
+    #   "1"=>{"name"=>"Acme", "industry"=>"Electronics"},
+    #   "2"=>{"name"=>"Best", "industry"=>"Software"}
+    # }
     def test_query
       @ss.process_query
       return md
     end
     
+    # Returns any errors stored in redis for the previous source adapter query
+    # For example: {"query-error"=>{"message"=>"error connecting to web service!"}}
     def query_errors
       @s.get_data(:errors)
     end
     
+    # Execute's the adapter's create method with a provided record and
+    # returns the object string from the create method.  If the create method
+    # returns a string, then a link will be saved for the device next time
+    # it synchronizes.  This link can be tested here.
+    #
+    # For example, in your spec:
+    # @product = {
+    #  'name' => 'iPhone',
+    #  'brand' => 'Apple',
+    #  'price' => '$299.99',
+    #  'quantity' => '5',
+    #  'sku' => '1234'
+    # }
+    # new_product_id = test_create(@product)
+    # create_errors.should == {}
+    # md[new_product_id].should == @product
+    # 
+    # This will return the result of the adapter's create method.  The master 
+    # document (:md) should also contain the new record.
     def test_create(record)
       @c.put_data(:create,{'temp-id' => record})
       @ss.create(@c.id)
@@ -39,24 +77,60 @@ module Rhosync
       links ? links['l'] : nil
     end
     
+    # Returns any errors stored in redis from the previous source adapter create
+    # (same structure as query errors)
     def create_errors
       @c.get_data(:create_errors)
     end
     
+    # Execute the source adapter's update method.
+    # Takes a record as hash of hashes (object_id => object)
+    # 
+    # For example:
+    # test_update({'4' => {'price' => '$199.99'}})
+    # update_errors.should == {}
+    # test_query
+    # md[product_id]['price'].should == '$199.99'
+    # 
+    # This will call the adapter's update method for object_id '4'
+    # NOTE: To test the master document, you will need to run def test_query
+    # as shown above
     def test_update(record)
       @c.put_data(:update,record)
       @ss.update(@c.id)
     end
     
+    # Returns any errors stored in redis from the previous source adapter update
+    # (same structure as query errors)
     def update_errors
       @c.get_data(:update_errors)
     end
     
+    # Execute the source adapter's delete method.
+    # Takes a record as hash of hashes (object_id => object)
+    #
+    # For example:
+    # @product = {
+    #   'name' => 'iPhone',
+    #   'brand' => 'Apple',
+    #   'price' => '$299.99',
+    #   'quantity' => '5',
+    #   'sku' => '1234'
+    # }
+    # test_delete('4' => @product)
+    # delete_errors.should == {}
+    # md.should == {}
+    # 
+    # This will call the adapter's delete method for product '4'
+    # NOTE: The master document (:md) will be updated and can be
+    # verified as shown above.
     def test_delete(record)
       @c.put_data(:delete,record)
       @ss.delete(@c.id)
     end
     
+    # Returns any errors stored in redis from the previous source adapter delete
+    # (same structure as query errors)
     def delete_errors
       @c.get_data(:delete_errors)
     end

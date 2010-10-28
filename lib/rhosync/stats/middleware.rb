f272448cc7b88c7cba7628915868a23d34909d93
lars <larsburgess@gmail.com>
2010-10-27 17:15:02 -0700

    * use PATH_INFO (for passenger)

--- lib/rhosync/stats/middleware.rb
+++ lib/rhosync/stats/middleware.rb
@@ -9,7 +9,7 @@ module Rhosync
         start = Time.now.to_f
         status, headers, body = @app.call(env)
         finish = Time.now.to_f
-        metric = "http:#{env['REQUEST_METHOD']}:#{env['REQUEST_PATH']}"
+        metric = "http:#{env['REQUEST_METHOD']}:#{env['PATH_INFO']}"
         source_name = env['rack.request.query_hash']["source_name"] if env['rack.request.query_hash']
         metric << ":#{source_name}" if source_name
         Record.save_average(metric,finish - start)

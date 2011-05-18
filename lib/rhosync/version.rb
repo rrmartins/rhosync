3cd264e78a89da7a611e5a56ee52f07951549dc1
lars <larsburgess@gmail.com>
2011-05-17 17:28:59 -0700

    * Bug fix for issue #13354369

--- lib/rhosync/version.rb
+++ lib/rhosync/version.rb
@@ -1,3 +1,3 @@
 module Rhosync
-  VERSION = '2.1.3'
+  VERSION = '2.1.4'
 end
--- spec/api/rhosync_api_spec.rb
+++ spec/api/rhosync_api_spec.rb
@@ -154,21 +154,22 @@ describe "RhosyncApi" do
   it "should list source attributes using direct api call" do
     RhosyncApi::get_source_params('',@api_token,"SampleAdapter").should == [
       {"name"=>"rho__id", "value"=>"SampleAdapter", "type"=>"string"}, 
-      {"name"=>"source_id", "value"=>nil, "type"=>"integer"}, 
-      {"name"=>"name", "value"=>"SampleAdapter", "type"=>"string"}, 
-      {"name"=>"url", "value"=>"", "type"=>"string"}, 
-      {"name"=>"login", "value"=>"", "type"=>"string"}, 
-      {"name"=>"password", "value"=>"", "type"=>"string"}, 
-      {"name"=>"priority", "value"=>3, "type"=>"integer"}, 
-      {"name"=>"callback_url", "value"=>nil, "type"=>"string"}, 
-      {"name"=>"poll_interval", "value"=>300, "type"=>"integer"}, 
-      {"name"=>"partition_type", "value"=>"user", "type"=>"string"}, 
-      {"name"=>"sync_type", "value"=>"incremental", "type"=>"string"}, 
-      {"name"=>"belongs_to", "type"=>"string", "value"=>nil},
-      {"name"=>"has_many", "type"=>"string", "value"=>"FixedSchemaAdapter,brand"},
-      {"name"=>"queue", "value"=>nil, "type"=>"string"}, 
-      {"name"=>"query_queue", "value"=>nil, "type"=>"string"}, 
-      {"name"=>"cud_queue", "value"=>nil, "type"=>"string"}]
+      {"name"=>"test_id", "value"=>nil, "type"=>"string"}] 
+      # {"name"=>"source_id", "value"=>nil, "type"=>"integer"}, 
+      # {"name"=>"name", "value"=>"SampleAdapter", "type"=>"string"}, 
+      # {"name"=>"url", "value"=>"", "type"=>"string"}, 
+      # {"name"=>"login", "value"=>"", "type"=>"string"}, 
+      # {"name"=>"password", "value"=>"", "type"=>"string"}, 
+      # {"name"=>"priority", "value"=>3, "type"=>"integer"}, 
+      # {"name"=>"callback_url", "value"=>nil, "type"=>"string"}, 
+      # {"name"=>"poll_interval", "value"=>300, "type"=>"integer"}, 
+      # {"name"=>"partition_type", "value"=>"user", "type"=>"string"}, 
+      # {"name"=>"sync_type", "value"=>"incremental", "type"=>"string"}, 
+      # {"name"=>"belongs_to", "type"=>"string", "value"=>nil},
+      # {"name"=>"has_many", "type"=>"string", "value"=>"FixedSchemaAdapter,brand"},
+      # {"name"=>"queue", "value"=>nil, "type"=>"string"}, 
+      # {"name"=>"query_queue", "value"=>nil, "type"=>"string"}, 
+      # {"name"=>"cud_queue", "value"=>nil, "type"=>"string"}]
   end
  
   it "should list source attributes using rest call" do

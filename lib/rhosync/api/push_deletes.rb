55860285fb5532304d0a152d0ce3d98f41462b17
lucas campbell-rossen <lucas@rhomobile.com>
2011-05-19 15:26:39 -0700

    fix params of api push

--- lib/rhosync/api/push_deletes.rb
+++ lib/rhosync/api/push_deletes.rb
@@ -1,7 +1,7 @@
 Server.api :push_deletes do |params,user|
   source = Source.load(params[:source_id],{:app_id=>APP_NAME,:user_id=>params[:user_id]})
   # if source does not exist create one for dynamic adapter
-  source = Source.create({:name => params[:source_name]},{:app_id => APP_NAME}) unless source
+  source = Source.create({:name => params[:source_id]},{:app_id => APP_NAME}) unless source
   source_sync = SourceSync.new(source)
   source_sync.push_deletes(params[:objects])
   'done'

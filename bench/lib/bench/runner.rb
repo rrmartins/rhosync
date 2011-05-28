c35c1c116558d4d1cb859585beb7a4a792a8ed69
Alexander Babichev <alexb@rhomobile.com>
2011-05-27 17:56:41 -0700

    Reworked code to create correct # of files with session dumps.

--- bench/lib/bench/runner.rb
+++ bench/lib/bench/runner.rb
@@ -8,32 +8,8 @@ module Bench
       @threads = []
       @sessions = []
     end
-    
+
     def test(concurrency,iterations,&block)
-#      total_time = time do
-#        0.upto(concurrency - 1) do |thread_id|
-#          sleep rand(2)                    
-#          threads << Thread.new(block) do |t|
-#            0.upto(iterations - 1) do |iteration|
-#              s = Session.new(thread_id, iteration)
-#              @sessions << s
-#              begin
-#                yield Bench,s
-#              rescue Exception => e
-#                puts "error running script: #{e.inspect}"
-#                puts e.backtrace.join("\n")
-#              end
-#            end
-#          end
-#        end
-#        begin 
-#          threads.each { |t| t.join }
-#        rescue RestClient::RequestTimeout => e
-#          bench_log "Request timed out #{e}"
-#        end
-#      end
-#      Bench.sessions = @sessions
-#      Bench.total_time = total_time      
       total_time = time do
         0.upto(concurrency - 1) do |thread_id|
           sleep rand(2)                    
@@ -47,25 +23,56 @@ module Bench
                 puts "error running script: #{e.inspect}"
                 puts e.backtrace.join("\n")
               end
-              File.open("/tmp/runner.#{pid}", "w+") { |f| Marshal.dump(s, f) }
+              File.open("/tmp/runner_#{iteration}.#{pid}", "w+") { |f| Marshal.dump(s, f) }
             end
           end
         end
         begin 
-          res = Process.waitall # returning an array of pid/status pairs
-          res.each do |ps| 
-            filename = "/tmp/runner.#{ps[0]}"
-            s = Marshal.load(File.open(filename))
-            @sessions << s
-            File.delete(filename)
+          @pid_stats = Process.waitall # returning an array of pid/status pairs
+        rescue RestClient::RequestTimeout => e
+          bench_log "Request timed out #{e}"
+        end
+      end
+      @pid_stats.each do |ps|
+        0.upto(iterations - 1) do |iteration|
+          filename = "/tmp/runner_#{iteration}.#{ps[0]}"
+          s = Marshal.load(File.open(filename))
+          @sessions << s
+          File.delete(filename)
+        end
+      end
+      Bench.sessions = @sessions
+      Bench.total_time = total_time
+    end
+
+=begin
+    def test(concurrency,iterations,&block)
+      total_time = time do
+        0.upto(concurrency - 1) do |thread_id|
+          sleep rand(2)                    
+          threads << Thread.new(block) do |t|
+            0.upto(iterations - 1) do |iteration|
+              s = Session.new(thread_id, iteration)
+              @sessions << s
+              begin
+                yield Bench,s
+              rescue Exception => e
+                puts "error running script: #{e.inspect}"
+                puts e.backtrace.join("\n")
+              end
+            end
           end
+        end
+        begin 
+          threads.each { |t| t.join }
         rescue RestClient::RequestTimeout => e
           bench_log "Request timed out #{e}"
         end
       end
       Bench.sessions = @sessions
       Bench.total_time = total_time
-    end
+    end      
+=end      
   end
 end  
   

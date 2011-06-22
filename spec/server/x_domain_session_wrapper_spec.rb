8fa4234927a5cfea2917c01c87e2dd159c723786
Dmitry Prokhorov <dipspb@gmail.com>
2011-06-22 23:38:13 +0400

    x_domain_session_wrapper URI patterns got parametrized, spec fixed

--- spec/server/x_domain_session_wrapper_spec.rb
+++ spec/server/x_domain_session_wrapper_spec.rb
@@ -12,10 +12,12 @@ COOKIE_ANOTHER_NV = "#{COOKIE_NAME}=#{COOKIE_VALUE}_another"
 PROPER_QUERY_STRING = "?abc=123&#{COOKIE_NAME}=#{CGI.escape(COOKIE_VALUE)}&de=45"
 WRONG_QUERY_STRING = "?abc=123&#{COOKIE_NAME}_wrong=#{CGI.escape(COOKIE_VALUE)}&de=45"
 
-PROPER_URI = '/application'
+PROPER_URI_NEW = '/api/application'
+PROPER_URI_OLD = '/application'
 WRONG_URI = '/some/wrong/path/to/rhosync/application'
 
-LOGIN_URI = '/application/clientlogin'
+LOGIN_URI_NEW = '/api/application/clientlogin'
+LOGIN_URI_OLD = '/application/clientlogin'
 
 describe "XDomainSessionWrapper middleware" do
 
@@ -27,15 +29,27 @@ describe "XDomainSessionWrapper middleware" do
 
   before(:each) do
     app ||= StubApp.new
-    @middleware = XDomainSessionWrapper.new(app, {:session_cookie => COOKIE_NAME})
+
+    @middleware_new_routes = XDomainSessionWrapper.new(app, {
+        :session_cookie => COOKIE_NAME,
+        :api_uri_regexp => /\A\/api\/application/,
+        :login_uri_regexp => /\A\/api\/application\/clientlogin/
+    })
+
+    @middleware_old_routes = XDomainSessionWrapper.new(app, {
+        :session_cookie => COOKIE_NAME,
+        :api_uri_regexp => /\A\/application/,
+        :login_uri_regexp => /\A\/application\/clientlogin/
+    })
+
   end
 
-  it "should skip if it isn't a sync protocol URI" do
+  it "should skip if it isn't a sync protocol URI, for new REST routes" do
     env = {
         'REQUEST_PATH' => WRONG_URI,
         'QUERY_STRING' => PROPER_QUERY_STRING
     }
-    status, headers, body = @middleware.call(env)
+    status, headers, body = @middleware_new_routes.call(env)
     200.should == status
     COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
     COOKIE_NV.should_not == env['HTTP_COOKIE']
@@ -43,12 +57,25 @@ describe "XDomainSessionWrapper middleware" do
     ''.should == body
   end
 
-  it "should process cookie from QUERY_STRING if it is a sync protocol URI" do
+  it "should skip if it isn't a sync protocol URI, for old REST routes" do
     env = {
-        'REQUEST_PATH' => PROPER_URI,
+        'REQUEST_PATH' => WRONG_URI,
         'QUERY_STRING' => PROPER_QUERY_STRING
     }
-    status, headers, body = @middleware.call(env)
+    status, headers, body = @middleware_old_routes.call(env)
+    200.should == status
+    COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
+    COOKIE_NV.should_not == env['HTTP_COOKIE']
+    headers['Content-Length'].should == body.length.to_s
+    ''.should == body
+  end
+
+  it "should process cookie from QUERY_STRING if it is a sync protocol URI, for new REST routes" do
+    env = {
+        'REQUEST_PATH' => PROPER_URI_NEW,
+        'QUERY_STRING' => PROPER_QUERY_STRING
+    }
+    status, headers, body = @middleware_new_routes.call(env)
     200.should == status
     COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
     env['HTTP_COOKIE'].should == COOKIE_VALUE
@@ -56,12 +83,25 @@ describe "XDomainSessionWrapper middleware" do
     ''.should == body
   end
 
-  it "shouldn't process cookie from QUERY_STRING if there is no appropriate parameter name or value" do
+  it "should process cookie from QUERY_STRING if it is a sync protocol URI, for old REST routes" do
     env = {
-        'REQUEST_PATH' => PROPER_URI,
+        'REQUEST_PATH' => PROPER_URI_OLD,
+        'QUERY_STRING' => PROPER_QUERY_STRING
+    }
+    status, headers, body = @middleware_old_routes.call(env)
+    200.should == status
+    COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
+    env['HTTP_COOKIE'].should == COOKIE_VALUE
+    headers['Content-Length'].should == body.length.to_s
+    ''.should == body
+  end
+
+  it "shouldn't process cookie from QUERY_STRING if there is no appropriate parameter name or value, for new REST routes" do
+    env = {
+        'REQUEST_PATH' => PROPER_URI_NEW,
         'QUERY_STRING' => WRONG_QUERY_STRING
     }
-    status, headers, body = @middleware.call(env)
+    status, headers, body = @middleware_new_routes.call(env)
     200.should == status
     COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
     env['HTTP_COOKIE'].should_not == COOKIE_VALUE
@@ -69,12 +109,38 @@ describe "XDomainSessionWrapper middleware" do
     ''.should == body
   end
 
-  it "should respond with cookie in a body if it is a login URI" do
+  it "shouldn't process cookie from QUERY_STRING if there is no appropriate parameter name or value, for old REST routes" do
     env = {
-        'REQUEST_PATH' => LOGIN_URI,
+        'REQUEST_PATH' => PROPER_URI_OLD,
+        'QUERY_STRING' => WRONG_QUERY_STRING
+    }
+    status, headers, body = @middleware_old_routes.call(env)
+    200.should == status
+    COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
+    env['HTTP_COOKIE'].should_not == COOKIE_VALUE
+    headers['Content-Length'].should == body.length.to_s
+    ''.should == body
+  end
+
+  it "should respond with cookie in a body if it is a login URI, for new REST routes" do
+    env = {
+        'REQUEST_PATH' => LOGIN_URI_NEW,
         'QUERY_STRING' => PROPER_QUERY_STRING
     }
-    status, headers, body = @middleware.call(env)
+    status, headers, body = @middleware_new_routes.call(env)
+    200.should == status
+    headers['Set-Cookie'].should == COOKIE_ANOTHER_NV
+    env['HTTP_COOKIE'].should == COOKIE_VALUE
+    headers['Content-Length'].should == body.length.to_s
+    ''.should_not == body
+  end
+
+  it "should respond with cookie in a body if it is a login URI, for old REST routes" do
+    env = {
+        'REQUEST_PATH' => LOGIN_URI_OLD,
+        'QUERY_STRING' => PROPER_QUERY_STRING
+    }
+    status, headers, body = @middleware_old_routes.call(env)
     200.should == status
     headers['Set-Cookie'].should == COOKIE_ANOTHER_NV
     env['HTTP_COOKIE'].should == COOKIE_VALUE

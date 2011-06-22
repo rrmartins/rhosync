85b9de5ca09919c8ca8455cddf620966d1c7deb8
Dmitry Prokhorov <dipspb@gmail.com>
2011-06-22 19:44:00 +0400

    x_domain_session_wrapper spec fixed

--- spec/server/x_domain_session_wrapper_spec.rb
+++ spec/server/x_domain_session_wrapper_spec.rb
@@ -5,18 +5,23 @@ require "cgi"
 
 COOKIE_NAME = 'some_cookie'
 COOKIE_VALUE = 'some_session_key=some_session_value'
-COOKIE_NAME_VALUE = "#{COOKIE_NAME}=#{COOKIE_VALUE}"
-COOKIE_NAME_ANOTHER_VALUE = "#{COOKIE_NAME}=#{COOKIE_VALUE}_another"
-QUERY_STRING = "?abc=123&#{COOKIE_NAME}=#{CGI.escape(COOKIE_VALUE)}&de=45"
-WRONG_URI = '/some/wrong/path/to/rhosync/application'
+
+COOKIE_NV = "#{COOKIE_NAME}=#{COOKIE_VALUE}"
+COOKIE_ANOTHER_NV = "#{COOKIE_NAME}=#{COOKIE_VALUE}_another"
+
+PROPER_QUERY_STRING = "?abc=123&#{COOKIE_NAME}=#{CGI.escape(COOKIE_VALUE)}&de=45"
+WRONG_QUERY_STRING = "?abc=123&#{COOKIE_NAME}_wrong=#{CGI.escape(COOKIE_VALUE)}&de=45"
+
 PROPER_URI = '/application'
+WRONG_URI = '/some/wrong/path/to/rhosync/application'
+
 LOGIN_URI = '/application/clientlogin'
 
 describe "XDomainSessionWrapper middleware" do
 
   class StubApp
     def call(env)
-      [200, {'Set-Cookie' => COOKIE_NAME_ANOTHER_VALUE, 'Content-Length' => '0'}, '']
+      [200, {'Set-Cookie' => COOKIE_ANOTHER_NV, 'Content-Length' => '0'}, '']
     end
   end
 
@@ -28,12 +33,12 @@ describe "XDomainSessionWrapper middleware" do
   it "should skip if it isn't a sync protocol URI" do
     env = {
         'REQUEST_PATH' => WRONG_URI,
-        'QUERY_STRING' => QUERY_STRING
+        'QUERY_STRING' => PROPER_QUERY_STRING
     }
     status, headers, body = @middleware.call(env)
     200.should == status
-    COOKIE_NAME_ANOTHER_VALUE.should == headers['Set-Cookie']
-    COOKIE_NAME_VALUE.should_not == env['HTTP_COOKIE']
+    COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
+    COOKIE_NV.should_not == env['HTTP_COOKIE']
     headers['Content-Length'].should == body.length.to_s
     ''.should == body
   end
@@ -41,25 +46,25 @@ describe "XDomainSessionWrapper middleware" do
   it "should process cookie from QUERY_STRING if it is a sync protocol URI" do
     env = {
         'REQUEST_PATH' => PROPER_URI,
-        'QUERY_STRING' => QUERY_STRING
+        'QUERY_STRING' => PROPER_QUERY_STRING
     }
     status, headers, body = @middleware.call(env)
     200.should == status
-    COOKIE_NAME_ANOTHER_VALUE.should == headers['Set-Cookie']
+    COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
     env['HTTP_COOKIE'].should == COOKIE_VALUE
     headers['Content-Length'].should == body.length.to_s
     ''.should == body
   end
 
-  it "should process cookie from QUERY_STRING if it is a sync protocol URI" do
+  it "shouldn't process cookie from QUERY_STRING if there is no appropriate parameter name or value" do
     env = {
         'REQUEST_PATH' => PROPER_URI,
-        'QUERY_STRING' => QUERY_STRING
+        'QUERY_STRING' => WRONG_QUERY_STRING
     }
     status, headers, body = @middleware.call(env)
     200.should == status
-    COOKIE_NAME_ANOTHER_VALUE.should == headers['Set-Cookie']
-    env['HTTP_COOKIE'].should == COOKIE_VALUE
+    COOKIE_ANOTHER_NV.should == headers['Set-Cookie']
+    env['HTTP_COOKIE'].should_not == COOKIE_VALUE
     headers['Content-Length'].should == body.length.to_s
     ''.should == body
   end
@@ -67,11 +72,11 @@ describe "XDomainSessionWrapper middleware" do
   it "should respond with cookie in a body if it is a login URI" do
     env = {
         'REQUEST_PATH' => LOGIN_URI,
-        'QUERY_STRING' => QUERY_STRING
+        'QUERY_STRING' => PROPER_QUERY_STRING
     }
     status, headers, body = @middleware.call(env)
     200.should == status
-    headers['Set-Cookie'].should == COOKIE_NAME_ANOTHER_VALUE
+    headers['Set-Cookie'].should == COOKIE_ANOTHER_NV
     env['HTTP_COOKIE'].should == COOKIE_VALUE
     headers['Content-Length'].should == body.length.to_s
     ''.should_not == body

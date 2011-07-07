aeb45a11ab941881e0eddb3664eae6720ee22751
lucas campbell-rossen <lucas@rhomobile.com>
2011-07-07 11:41:33 -0700

    remove puts

--- lib/rhosync/api/delete_user.rb
+++ lib/rhosync/api/delete_user.rb
@@ -3,7 +3,6 @@ Server.api :delete_user do |params,user|
   App.load(APP_NAME).users.delete(params[:user_id])
   params = {:app_id => APP_NAME,:user_id => params[:user_id]}
   App.load(APP_NAME).sources.members.each{ |source|
-    puts 'deleteing source users ------'
     Source.load(source, params).flash_store_data
   }
   "User deleted"
--- lib/rhosync/document.rb
+++ lib/rhosync/document.rb
@@ -22,7 +22,6 @@ module Document
   end
   
   def flash_data(doctype)
-    puts docname(doctype)
     Store.flash_data(docname(doctype))
   end
   
